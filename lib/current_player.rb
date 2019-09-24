7fcaa97381c29b705d95514bc9632729236c6c21
Russell Macalintal <russell.macalintal@gmail.com>
2019-09-24 02:57:28 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,10 @@
 def turn_count(game_board)
-
+  counter = 0
+  game_board.each do |token|
+    if token == "X" || token == "O"
+      counter += 1
+    end
+  end
 end
 
 def current_player(game_board)
